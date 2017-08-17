5dd34982eb635eae28b7fb8367af01b5df5105f5
Aaron Drobek <adrobek09@gmail.com>
2017-08-16 22:44:18 -0400

    app is operational, maybe look into some more styling options

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
-const express = ("express");
+const express = require("express");
 const router = express.Router();
 
 
+const task = ["wash the car", "vacuum the carpet", "pack the kids' lunches", "go grocery shopping", "tune up the hemi",  "jack-hammer side walk", "have five daiquiri's"];
+const complete = ["build a dynamic list maker"];
 
+router.use("/public", express.static("public"));
+
+router.get("/", function(req, res){
+res.render('main', {task: task, complete: complete});
+});
+
+router.post("/", function(req, res){
+  task.push(req.body.todo_input);
+  console.log(task);
+  res.redirect("/");
+})
+
+router.post("/complete", function(req, res){
+  complete.push(task.splice(task.indexOf(req.body.click_done),1));
+  res.redirect("/");
+})
 
 
 
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app              = express();
 app.engine("mustache", mustacheExpress());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "mustache");
-app.set("layout", "layout");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));

cd97085b9a72dcb4513dc0a0a566380179a46d98
Aaron Drobek <adrobek09@gmail.com>
2017-08-30 16:30:38 -0400

    deadline

---
+++ migrations/20170830202421-create-task.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      todo_input: {
+        type: Sequelize.STRING
+      },
+      isComplete: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Tasks');
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,67 @@
 const express = require("express");
 const router = express.Router();
+const models = require("../models/index");
 
 
-const task = ["wash the car", "vacuum the carpet", "pack the kids' lunches", "go grocery shopping", "tune up the hemi",  "jack-hammer side walk", "have five daiquiri's"];
-const complete = ["build a dynamic list maker"];
+
 
 router.use("/public", express.static("public"));
 
-router.get("/", function(req, res){
-res.render('main', {task: task, complete: complete});
+// router.get("/", function(req, res){
+// res.render('main', {task: task, complete: complete});
+// });
+router.get("/", function(req,res){
+  models.Task.findAll()
+  .then(function(data) {
+    console.log(data,"data here");
+    res.render("main", {task: data});
+  })
 });
 
-router.post("/", function(req, res){
-  task.push(req.body.todo_input);
-  console.log(task);
-  res.redirect("/");
-})
 
-router.post("/complete", function(req, res){
-  complete.push(task.splice(task.indexOf(req.body.click_done),1));
-  res.redirect("/");
-})
+router.post("/", function(req,res){
+  models.Task.create({
+    todo_input: req.body.todo_input
+  })
+  .then(function(data) {
+    res.redirect("/");
+  })
+});
+router.get("/destroy/:id", function(req,res){
+  models.Task.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(function(data) {
+    res.redirect("/");
+  })
+});
+
+router.post("/edit/:id", function(req,res){
+  models.Task.update({
+    todo_input: req.body.todo_input,
+  },{
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(function(data) {
+    res.redirect("/");
+  })
+});
+
+//
+// router.post("/complete/:id", function(req, res){
+//   models.Task.findOne()
+//   console.log(task);
+//   res.render("/below", {task: data});
+// })
+//
+// router.post("/complete", function(req, res){
+//   complete.push(task.splice(task.indexOf(req.body.click_done),1));
+//   res.redirect("/");
+// })
 
 
 
